a056785728710de6d6b42ca725fab02aae63cf9a
Midhun <midhun.george777@gmail.com>
2024-05-30 11:34:08 +0100

    responsive design

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit{
   
-  
+  isMobile = window.innerWidth <= 1023;
+
+  constructor(private el: ElementRef) { }
+
+  ngOnInit(): void {
+    
+  }
+  hideMenu() {
+    if (window.innerWidth <= 1023) {
+        $("input").prop("checked", "");
+      
+    }
+  }
+
 }
